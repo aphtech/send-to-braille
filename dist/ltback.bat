9f43a7f8e415c691b431f8f5778e90a18f9816f5
Timothy Wynn <twynn92@gmail.com>
2017-10-16 18:51:50 -0400

    Redirected sterr's output to log file(s) in the ^%temp^% directory; added --wrap=preserve parameter to Pandoc's invocation to prevent default wrapping of 72 characters per line.

--- dist/ltback.bat
+++ dist/ltback.bat
@@ -8,7 +8,7 @@ rem Set the translation table, this one is English UEB contracted
 set table=en-ueb-g2.ctb
 rem call %x%settable.bat
 
-type %1|%x%lou_translate %louflg% %table%> %1.txt
+type %1|%x%lou_translate %louflg% %table%> %1.txt 2> %temp%\lou_translate.log
 rem Open editor. Rem the following line to skip this step
 rem %x%bz.jar "%~1.brl"
 rem if you want, add a line like the one below to copy to a card. Set target to your drive
