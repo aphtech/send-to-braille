a1560dc704dd61e956bacf4841fe8f5a48540937
Timothy Wynn <twynn92@gmail.com>
2018-02-14 20:10:56 -0500

    Avoid UTF-8 reencoding when appropriate.

--- dist/lt1.bat
+++ dist/lt1.bat
@@ -6,11 +6,20 @@ set louis_tablepath=%x%tables\
 rem Set the translation table, this one is English UEB contracted
 set table=en-ueb-g1.ctb
 rem call %x%settable.bat
-rem pandoc to text then LouTran to braille with .brl appended to original file name
-copy %1 "%temp%\Utf8n%~x1"
-%x%Utf8n "%temp%\Utf8n%~x1"
-%x%pandoc -t plain --wrap=preserve "%temp%\Utf8n%~x1" 2> %temp%\pandoc.log|%x%lou_translate %table% > %1.brl 2> %temp%\lou_translate.log
-del "%temp%\Utf8n%~x1"
+
+rem pandoc to text then LouTran to braille with .brl appended to original file name, avoiding UTF-8 encoding when appropriate:
+set utf8=false
+if not "%~x1"==".docx" if not "%~x1"==".epub" if not "%~x1"==".odt" (
+    set utf8=true
+    copy %1 "%temp%\Utf8n%~x1"
+    %x%Utf8n "%temp%\Utf8n%~x1"
+    %x%pandoc -t plain --wrap=preserve "%temp%\Utf8n%~x1" 2> %temp%\pandoc.log|%x%lou_translate %table% > %1.brl 2> %temp%\lou_translate.log
+    del "%temp%\Utf8n%~x1"
+)
+if "%utf8%"=="false" (
+    %x%pandoc -t plain --wrap=preserve %1 2> %temp%\pandoc.log|%x%lou_translate %table% > %1.brl 2> %temp%\lou_translate.log
+)
+
 rem Open editor. Rem the following line to skip this step
 rem %x%bz.jar "%~1.brl"
 rem if you want, add a line like the one below to copy to a card. Set target to your drive
