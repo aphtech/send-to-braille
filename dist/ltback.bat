96bec63c0817e40fb62f8171f86601a31e56b1ff
Timothy Wynn <tmthywynn8@gmail.com>
2022-03-16 22:00:02 -0400

    Updated ltback.bat to loopp through files with shift

--- dist/ltback.bat
+++ dist/ltback.bat
@@ -8,9 +8,12 @@ rem Set the translation table, this one is English UEB contracted
 set table=en-ueb-g2.ctb
 rem call %x%settable.bat
 
+:LOOP
 type %1|%x%lou_translate %louflg% %table%> %1.txt 2> %temp%\lou_translate.log
 rem Open editor. Rem the following line to skip this step
 rem %x%bz.jar "%~1.brl"
 rem if you want, add a line like the one below to copy to a card. Set target to your drive
 set target=f:
 rem copy "%~1.brl" target
+shift
+if not [%1]==[] goto :Loop
